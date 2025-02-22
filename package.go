528a780653375515a38a969eff711a3c4d38a329
geekip <mail@yangfei.site>
2025-02-23 00:48:22 +0800

    Update package info

--- package.go
+++ package.go
@@ -1,8 +1,11 @@
+// Copyright 2024 Geekip. All rights reserved.
+// Use of this source code is governed by a MIT style.
+// at https://github.com/geekip/lug
+
 package main
 
 import lua "github.com/yuin/gopher-lua"
 
-// GopherLua: VM and compiler for Lua in Go
 const PackageName = "lug"
 const PackageVersion = "1.0.0"
 const PackageAuthors = "Geekip"
